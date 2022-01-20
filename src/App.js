6bfb1334054736a7118d73abaf8c057d82d0d429
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-01-20 02:01:05 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Poem from './components/Poem'
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Love Me, Love Me Not</h1>
+      <header>
+        <p>love-letters-and-poetry</p>
+        <p>data from <a href='https://poetrydb.org/index.html' >poetrydb.org</a></p>
+      </header>
+      <div className='title'>
+        <h1><span className='love'>Love</span> me, <span className='not'>Love me</span> not</h1>
         <h2> poems to inspire your next love or not love letter.</h2>
       </div>
       <Poem />
--- src/components/Poem.js
+++ src/components/Poem.js
@@ -16,7 +16,6 @@ const Poem = ({ poem, isFetching, error, dispatch})=>{
         return <h3>poem loading...</h3>;
     }
 
-
     const handleLoveClick = ()=>{
         dispatch(getPoem('love'))
     }
@@ -27,38 +26,18 @@ const Poem = ({ poem, isFetching, error, dispatch})=>{
 
     return (
         <div>
-            <div>
-              <button onClick={handleLoveClick}>Love Me</button >
-              <button onClick={handleNoLoveClick}>Love Me Not</button>  
+            <div className='buttons'>
+              <button onClick={handleLoveClick}>love me</button >
+              <button onClick={handleNoLoveClick}>love me not</button>  
             </div>
         {poem && 
-            <div>
-                <h3>{poem.title}</h3>
-                <h3>{poem.author}</h3>
+            <div className='poemContainer'>
+                <h3 className='poemTitle'>{poem.title}</h3>
+                <h3 className='author'>{`By ${poem.author}`}</h3>
                 <p>{poem.lines.join('\n')}</p>
             </div>
             }
-        {/* <div>
-            
-            <h3>Sonnet 12 - Indeed this very love which is my boast"</h3>
-            <h3>By Elizabeth Barrett Browning</h3>
-            <p>
-            "Indeed this very love which is my boast,",
-        "And which, when rising up from breast to brow,",
-        "Doth crown me with a ruby large enow",
-        "To draw men's eyes and prove the inner cost,—",
-        "This love even, all my worth, to the uttermost,",
-        "I should not love withal, unless that thou",
-        "Hadst set me an example, shown me how,",
-        "When first thine earnest eyes with mine were crossed,",
-        "And love called love. And thus, I cannot speak",
-        "Of love even, as a good thing of my own:",
-        "Thy soul hath snatched up mine all faint and weak,",
-        "And placed it by thee on a golden throne,—",
-        "And that I love (O soul, we must be meek!)",
-        "Is by thee only, whom I love alone."
-            </p>
-        </div> */}
+       
       </div>
     )
 }
